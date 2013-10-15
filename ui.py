fca3dae5403ff5a0b20aae8d537247986546fd19
Ron <ronb@ironsrc.com>
2013-10-15 14:03:37 +0200

    new ui changes

--- ui.py
+++ ui.py
@@ -1,11 +1,14 @@
 from flask import Flask
 from flask import render_template
+# from security_groups_manager import IpTablesManager
 app = Flask(__name__)
 
-@app.route('/<name>')
-def hello_world(name=None):
+@app.route('/')
+def hello_world():
+	# post request to server to get ip blacklist
     # return 'Hello World!'
-    return render_template('ui.html', name=name)
+    blacklist = [{'ip' : '8.8.8.8/255.255.255.255'}]
+    return render_template('ui.html', blacklist = blacklist)
 
 if __name__ == '__main__':
     app.run()
