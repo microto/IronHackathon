826f3ff08668e33d504adcd74b289c89a5d11f7e
Shimon Tolts <Shimont@ironSrc.com>
2013-10-15 15:10:35 +0300

    ui

--- ui.py
+++ ui.py
@@ -1,14 +1,24 @@
 from flask import Flask
+import flask
 from flask import render_template
 # from security_groups_manager import IpTablesManager
 app = Flask(__name__)
+app.config.from_object(__name__)
 
-@app.route('/')
-def hello_world():
-	# post request to server to get ip blacklist
-    # return 'Hello World!'
-    blacklist = [{'ip' : '8.8.8.8/255.255.255.255'}]
-    return render_template('ui.html', blacklist = blacklist)
+
+class IronWeb(flask.views.MethodView):
+    def get(self):
+        # post request to server to get ip blacklist
+        # return 'Hello World!'
+        blacklist = [{'ip' : '8.8.8.8/255.255.255.255'}]
+        return render_template('ui.html', blacklist = blacklist)
+
+    def post(self):
+        #add to whitelist
+        pass
+
+app.add_url_rule('/', view_func=IronWeb.as_view('main'), methods=['GET', 'POST'])
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = True
+    app.run(host="0.0.0.0", port=80)
