d78e2549a1f6a077f2cd244900c6624589e52ce7
Roman Labunsky <romanl@ironsrc.com>
2013-10-15 13:54:06 +0300

    changed to iptables_manager

--- iptables_manager.py
+++ iptables_manager.py
@@ -3,18 +3,19 @@ __author__ = 'romanl'
 import iptc
 ADD_TEMPLATE = '{ip}/255.255.255.255'
 
+
 class IpTablesManager(object):
-    def __init__(self):
-        self._chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
+
     def add_ips_to_block_list(self, ips):
-        #chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
+
         for ip in ips:
+            chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
             rule = iptc.Rule()
             rule.in_interface = 'eth+'
             rule.src = ADD_TEMPLATE.format(ip=ip)
             target = iptc.Target(rule, 'DROP')
             rule.target = target
-            self._chain.insert_rule(rule=rule)
+            chain.insert_rule(rule=rule)
 
     def remove_ips_from_block_list(self, ips):
         pass
