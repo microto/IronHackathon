e7ff6b0c23a8777d11aa9f3cb0a981f75b413735
root <root@romanl-dell-linux.(none)>
2013-10-15 14:34:28 +0300

    Fixed iptables

--- iptables_manager.py
+++ iptables_manager.py
@@ -7,21 +7,28 @@ ADD_TEMPLATE = '{ip}/255.255.255.255'
 class IpTablesManager(object):
 
     def add_ips_to_block_list(self, ips):
-
+        import subprocess
+        counter = 0
         for ip in ips:
-            chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
-            rule = iptc.Rule()
-            rule.in_interface = 'eth+'
-            rule.src = ADD_TEMPLATE.format(ip=ip)
-            target = iptc.Target(rule, 'DROP')
-            rule.target = target
-            chain.insert_rule(rule=rule)
+            subprocess.call('iptables -I INPUT -s {0} -j DROP'.format(ip), shell=True)
+            counter += 1
+        print 'Added {0} Rules to table'.format(counter)
+        #for chain_counter in xrange(len(ips)/500):
+        #    chain = iptc.Chain(iptc.Table(iptc.Table.FILTER), "INPUT")
+        # for ip in ips[chain_counter * 500:(chain_counter + 1) * 500]:
+
+                #rule = iptc.Rule()
+                #rule.in_interface = 'eth+'
+                #rule.src = ADD_TEMPLATE.format(ip=ip)
+                #target = iptc.Target(rule, 'DROP')
+                #rule.target = target
+                #chain.append_rule(rule=rule)
 
     def remove_ips_from_block_list(self, ips):
         pass
 
     def get_blacklist(self):
-        return self._chain.rules
+        return self.rules
 
     def get_whitelist(self):
         pass
